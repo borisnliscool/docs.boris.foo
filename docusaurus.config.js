93d59f4970c2a83dc8e881444198ca0370c30a9a
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2023-07-05 23:31:12 +0200

    fix edit button

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
 					sidebarPath: require.resolve("./sidebars.js"),
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
-					editUrl: "https://github.com/borisnliscool/docs.boris.foo",
+					editUrl: "https://github.com/borisnliscool/docs.boris.foo/tree/main/",
 				},
 				theme: {
 					customCss: require.resolve("./src/css/custom.css"),
